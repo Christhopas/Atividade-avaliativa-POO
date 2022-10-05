218eb0592a3689064fdfb4f991921c2964d96bc2
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-10-05 13:58:22 -0300

    Update Main.java
    Manter as instâncias e a atribuição "oraculo.guerreiro = joao;"

--- Main.java
+++ Main.java
@@ -5,6 +5,14 @@ public class Main {
         
         Guerreiro joao = new Guerreiro();
         Oraculo oraculo = new Oraculo();
+        
         oraculo.guerreiro = joao;
+        
+        joao.setNome("joao");
+        oraculo.setNome("Oraculo");
+        oraculo.setVidas();
+        System.out.println(joao.vidas);
+        oraculo.loadLevel1();
+        oraculo.loadLevel2(0);
     }
 }
