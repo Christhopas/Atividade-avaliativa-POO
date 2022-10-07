7c528e2eb745c41161519723783b01f55b54490e
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-10-07 07:30:07 -0300

    Apagar após download para envio via comandos git
    Upando arquivos para download do grupo

---
+++ E-comerce/Loja.java
@@ -0,0 +1,25 @@
+
+public class Loja {
+    
+    private Pedido listaPedido;
+    
+    public Loja() {
+        this.listaPedido = new Pedido();
+    }
+
+    public void menu() {
+        System.out.println("Funcoes: ");
+        System.out.println("inserirPedido()");
+        System.out.println("buscarPedido()");
+        System.out.println("removerPedido()");
+    }
+    public void inserirPedido() {
+    }
+    public void buscarPedido() {
+    }
+    public void removerPedido() {
+    }
+    
+    public static void main(String[] args) {
+    }
+}
---
+++ E-comerce/Pedido.java
@@ -0,0 +1,14 @@
+
+import java.util.Date;
+
+public class Pedido {
+    
+    private int pedidoID;
+    private Date dataEmissao;
+    private float total;
+    
+    public Pedido() {
+        this.dataEmissao = new Date();
+    }
+
+}
