ae6f7ad8bd3862d2dee0f0fda213b2f9663f4c53
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-10-19 10:48:37 -0300

    Update Funcionario.java

--- E-comerce/Funcionario.java
+++ E-comerce/Funcionario.java
@@ -1,6 +1,21 @@
-
 public class Funcionario {
     
-    private String nome;
+    public String nome;
     private int matricula;
+
+    public void getNome() {
+        System.out.println(this.nome);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
 }
