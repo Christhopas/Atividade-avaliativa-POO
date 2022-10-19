a90ca4e002dc74bd3eef7dc255f85c557df59b51
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-10-19 10:37:48 -0300

    Update Pedido.java

--- E-comerce/Pedido.java
+++ E-comerce/Pedido.java
@@ -1,14 +1,21 @@
-
 import java.util.Date;
 
 public class Pedido {
     
-    private int pedidoID;
+    public int pedidoID;
     private Date dataEmissao;
     private float total;
+    private int qtd;
     
-    public Pedido() {
+    public Pedido(int pedidoID, float total, int qtd) {
+        this.pedidoID = pedidoID;
+        this.total = total;
+        this.qtd = qtd;
         this.dataEmissao = new Date();
     }
-
+    
+    public void totalCalculado() {
+        float totalCalculado = this.total * this.qtd;
+        System.out.println("O total do seu pedido e R$"+totalCalculado);
+    }
 }
