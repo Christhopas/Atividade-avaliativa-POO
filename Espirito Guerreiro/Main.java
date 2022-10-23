2ca4cccdeee69636f86c879c4b4315bdf7cabdb9
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-10-22 21:13:22 -0300

    commit

--- Espirito Guerreiro/Main.java
+++ Espirito Guerreiro/Main.java
@@ -2,19 +2,12 @@ public class Main {
     
     public static void main(String[] args) {
         
-        Guerreiro joao = new Guerreiro();
+        Guerreiro vinicius = new Guerreiro();
         Oraculo oraculo = new Oraculo();
         
-        oraculo.guerreiro = joao;
+        oraculo.guerreiro = vinicius;
         
-        joao.setNome("joao");
+        vinicius.setNome("Vinicius");
         oraculo.setNome("Oraculo");
-        oraculo.setVidas();
-        System.out.println("Voce tem:   " + joao.vidas + " vidas");
-        oraculo.loadLevel1();
-        
-        if(oraculo.checkVitoria == 1) {     
-            oraculo.loadLevel2(0); // 0 para PAR e 1 para IMPAR
-        }
     }
 }
