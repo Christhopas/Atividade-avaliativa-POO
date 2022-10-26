ac3dc2404f5c133d789fffc00776c54e1dbf52f0
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2022-10-26 09:31:22 -0300

    Update Loja.java

--- E-comerce/Loja.java
+++ E-comerce/Loja.java
@@ -2,9 +2,6 @@ import java.util.HashMap;
 
 public class Loja {
     
-    //Funcionario funcionario = new Funcionario();
-    //HashMap<Integer, Integer> listaFuncionario = new HashMap<>();
-    
     HashMap<Integer, Pedido> listaPedido = new HashMap<>();
 
     public void menu() {
@@ -34,29 +31,5 @@ public class Loja {
     }
     
     public static void main(String[] args) {
-        
-        Pedido p1 = new Pedido();
-        p1.inserirItem("Melancia", 1);
-        p1.inserirItem("Kiwi", 5);
-        p1.inserirItem("Goiaba", 3);
-        p1.buscarItem("Melancia");
-        p1.buscarItem("Kiwi");
-        p1.removerItem("Goiaba");
-        p1.trocarQtd("Melancia", 2);
-        p1.buscarQtd("Melancia");
-        p1.verLista();
-        p1.limparLista();
-        Pedido p2 = new Pedido();
-        Pedido p3 = new Pedido();
-        Pedido p4 = new Pedido();
-        
-        Loja americanas = new Loja(); 
-        americanas.inserirPedido(1, p1);
-        americanas.inserirPedido(2, p2);
-        americanas.inserirPedido(3, p3);
-        americanas.verLista();
-        americanas.trocarPedido(2, p4);
-        americanas.verLista();
-        americanas.limparLista();
     }
 }
